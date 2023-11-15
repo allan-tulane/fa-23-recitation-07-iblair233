2274c4e04119de4112311d7003595c787e920a6e
iblair233 <126489949+iblair233@users.noreply.github.com>
2023-11-15 17:37:19 -0600

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,11 @@ def reachable(graph, start_node):
     frontier = set([start_node])
     while len(frontier) != 0:
         ###TODO
-        pass
+        v = frontier.pop()
+        result.add(v)
+        for i in graph[v]:
+          if i not in result:
+            frontier.add(i)
     return result
 
 
@@ -27,8 +31,13 @@ def reachable(graph, start_node):
 
 def connected(graph):
     ### TODO
-    pass
-
+    keys = list(graph.keys())[0]
+    graph_len = len(graph)
+    reach_length = len(reachable(graph, keys))
+    if graph_len == reach_length:
+      return True
+    else:
+      return False
 
 
 
@@ -38,5 +47,13 @@ def n_components(graph):
       the number of connected components in an undirected graph
     """
     ### TODO
-    pass
+    components = []
+    frontier = set(list(graph.keys()))
+    while len(frontier) != 0:
+      visited = frontier.pop()
+      graphreach = reachable(graph, visited)
+      frontier = frontier - graphreach
+      components.append(graphreach)
+    return len(components)
+    
 
